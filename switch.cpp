b93e46abc3ef6b871e85fa51accd0f8edc3ab2a1
RishabhRanjanmiw <rishabhranjan177@gmail.com>
2025-08-07 17:14:14 +0530

    This is one project and some changes in switch case

--- switch.cpp
+++ switch.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
  int main(){
-    int num = 5;
+    int num = 2;
     char ch = '3';
     cout << endl;
 
-    switch (num) {
-        case 1: cout<< "First" << endl;
+    switch (2*num) {
+        case 4: cout<< "First" << endl;
         break;
-        case 5:switch(ch){
+        case 1:switch(ch){
             case '2':cout<<" Value of ch " << ch << endl;
             case '3': cout << "Value of ch " << ch << endl;
         }
